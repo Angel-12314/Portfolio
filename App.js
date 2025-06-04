7df09a7df5ee23f839eb9da5d10b7c3c11c1c395
Angel-12314 <59677750+Angel-12314@users.noreply.github.com>
2025-06-04 19:00:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Header from "./Components/Header"
+import Hero from "./Components/Hero"
+import About from "./Components/About"
+import Experience from "./Components/Experience"
+import Skills from "./Components/Skills"
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <About />
+      <Experience />
+      <Skills />
+    </div>
+  );
+}
+
+export default App;
